232be1872dd4f54c6b314ea8d15dc287c99fcc06
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-02-12 21:31:32 +0100

    feee

--- public/components/js/main/Main.js
+++ public/components/js/main/Main.js
@@ -3,7 +3,20 @@ import styles from "/_repos/portfolio/styles/css/Main.module.css";
 function Main() {
   return (
     <section className={styles.main}>
-      <div className={styles.main_home}>aedwed</div>
+      <div className={styles.main_home}>
+        <div className={styles.overlay}></div>
+        <div className={styles.shadow_overlay}></div>
+        <div className={styles.home_top_content}>
+          <div className={styles.home_top_left_content}>
+            <div>
+              <div className={styles.home_hero_line}></div>
+              <span>HELLO THERE</span>
+            </div>
+          </div>
+          <div className={styles.home_top_right_content}></div>
+        </div>
+        <div className={styles.home_bottom_content}></div>
+      </div>
       <div className={styles.main_about}>about</div>
       <div className={styles.main_work}>work</div>
       <div className={styles.main_contact}>contact</div>
